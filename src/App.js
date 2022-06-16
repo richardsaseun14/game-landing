a25a557f444a47b360352d41e70f4ca99fa09321
Richard Saseun <richardsaseun@gmail.com>
2022-06-16 03:13:55 +0100

    chore: create header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from './components/header';
+
 function App() {
   return (
     <div className="h-screen bg-[#171954] bg-home text-white">
+      <Header></Header>
       <h1 className=" text-3xl font-bold underline">Hello world!</h1>
       <p className="">Na paragraph be dis</p>
       <div className="w-2/3 h-2/3 bg-glass"></div>
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../assets/img/ta-logo.svg';
+import calIcon from '../assets/img/ta-calendar-icon.svg';
+
+const Header = () => {
+  return (
+    <div className="px-5 md:px-28 py-9 flex justify-between items-center">
+      <img src={logo} alt="" />
+      <div className="flex items-center">
+        <img height="90px" src={calIcon} alt="" className="mr-2" />
+        <div className="">
+          <p className="font-bold text-lg">Day 1</p>
+          <p className="text-sm opacity-60">of 5</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
