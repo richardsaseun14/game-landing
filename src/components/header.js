2422b48c5a59189bd19d7f3489df4d87bb1d3835
Richard Saseun <richardsaseun@gmail.com>
2022-06-17 16:12:15 +0100

    chore: check fix for delayed image loading

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from '../assets/img/ta-logo.svg';
-import calIcon from '../assets/img/ta-calendar-icon.svg';
+// import logo from '../assets/img/ta-logo.svg';
+// import calIcon from '../assets/img/ta-calendar-icon.svg';
 
 const Header = () => {
   return (
     <div className="px-5 md:px-28 py-2 md:py-5 flex justify-between items-center">
-      <img src={logo} alt="" />
+      <img src="/assets/ta-logo.svg" alt="" />
       <div className="flex items-center">
-        <img height="90px" src={calIcon} alt="" className="mr-2" />
+        <img
+          height="90px"
+          src="/assets/ta-calendar-icon.svg"
+          alt=""
+          className="mr-2"
+        />
         <div className="">
           <p className="font-bold text-lg">Day 1</p>
           <p className="text-sm opacity-60">of 5</p>
