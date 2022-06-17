88c874b6954748d45145fb0ba7bda97788af3983
Richard Saseun <richardsaseun@gmail.com>
2022-06-17 16:09:33 +0100

    chore: add favicon and adjust text animation

--- src/components/hero-text.js
+++ src/components/hero-text.js
@@ -4,22 +4,22 @@ const HeroText = () => {
   return (
     <>
       <div className="overflow-hidden">
-        <h2 className="text-3xl md:text-7xl animate-[slideInx2_1s_ease-in-out_1s_both]">
+        <h2 className="text-3xl md:text-7xl mt-3 md:mt-0 animate-[slideInx2_1s_ease-in-out_0.5s_both]">
           Unlock to
         </h2>
       </div>
       <div className="overflow-hidden">
-        <h2 className="text-3xl md:text-7xl font-bold animate-[slideInx2_1s_ease-in-out_1.1s_both]">
+        <h2 className="text-3xl md:text-7xl font-bold animate-[slideInx2_1s_ease-in-out_0.7s_both]">
           Power your dreams!
         </h2>
       </div>
       <div className="overflow-hidden">
-        <p className="md:text-2xl md:mt-6 mt-3 font-light animate-[slideInx2_1s_ease-in-out_1.2s_both]">
+        <p className="md:text-2xl md:mt-6 mt-3 font-light animate-[slideInx2_1s_ease-in-out_1s_both]">
           Stand a chance to win{' '}
         </p>
       </div>
       <div className="overflow-hidden">
-        <p className="md:text-2xl font-light animate-[slideInx2_1s_ease-in-out_1.3s_both]">
+        <p className="md:text-2xl font-light animate-[slideInx2_1s_ease-in-out_1.1s_both]">
           <span className="font-extrabold"> N3,000,000</span> everyday for the
           next 5 days
         </p>
